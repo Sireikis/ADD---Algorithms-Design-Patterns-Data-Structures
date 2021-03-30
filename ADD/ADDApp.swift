af0abc582b5baa06bf66c2e19a0cf466429e2e1c
Ignas Sireikis <iscode@yahoo.com>
2021-03-30 14:04:04 -0500

    Initial Commit

---
+++ ADD/ADDApp.swift
@@ -0,0 +1,17 @@
+//
+//  ADDApp.swift
+//  ADD
+//
+//  Created by Ignas Sireikis on 3/30/21.
+//
+
+import SwiftUI
+
+@main
+struct ADDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
